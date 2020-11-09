11a80efe1fc90e4a6e4bb5f8b257bccc3b8706b6
Simona Cinti <simona.cinti2@gmail.com>
2020-11-09 15:55:24 +0100

    next photo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('hello', Vue);
+// console.log('hello', Vue);
+
+/**
+ * Vue Slider
+ */
+
+ const  app = new Vue ({
+     el: '#app',
+
+     data: {
+        indexPhoto: 0,
+        photos: [
+            './img/image1.jpg',
+            './img/image2.jpg',
+            './img/image3.jpg',
+            './img/image4.jpg'
+        ]
+     },
+     methods: {
+        nextPhoto() {
+            this.indexPhoto ++
+        }
+     }
+
+ })
