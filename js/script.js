96e0f86bf7d5457645aa14769fa71637ab317a30
Simona Cinti <simona.cinti2@gmail.com>
2020-11-09 16:38:32 +0100

    mouse in&out

--- js/script.js
+++ js/script.js
@@ -43,7 +43,10 @@
         startLoop(){
             this.loopId = setInterval(() =>{
                 this.nextPhoto();
-            }, 3000);
+            }, 3000); //debug 1000
+        },
+        stopLoop(){
+            clearInterval(this.loopId);
         }
     }
 
